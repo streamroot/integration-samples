4bc85b8793dc62028e107678cb9299ff4381b5d8
Boris Borgobello <boris.borgobello@lumen.com>
2022-08-08 13:11:31 +0700

    wip

---
+++ orchestrator/Apple/AVPlayerPlugin/AVPlayerOrchestrator/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  AVPlayerOrchestrator
+//
+//  Created by Ndiaye, Lamine on 19/08/2020.
+//  Copyright © 2020 Streamroot. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    LMDeliveryClient.initializeApp()
+    return true
+  }
+}
